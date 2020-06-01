7bbd0e93e0c301351e303640f92a28d3dbe19929
moodymudskipper <antoine.fabri@gmail.com>
2020-06-02 01:10:54 +0200

    document methods

--- R/methods.R
+++ R/methods.R
@@ -1,38 +1,54 @@
 # this is to avoid creating elephant objects when using `<-` and expressions
 # which would preserve class and attributes
 
+#' methods for class 'elephant'
+#' @param x object
+#' @param ... passed to other methods
+#' @param e1 object
+#' @param e2 object
+#' @rdname elephant_methods
 #' @export
 Math.elephant <- function(x, ...){
   x <- forget(x)
   NextMethod()
 }
 
+#' @rdname elephant_methods
 #' @export
 Ops.elephant <- Math.elephant
 
+#' @rdname elephant_methods
 #' @export
 Complex.elephant <- Math.elephant
 
+#' @rdname elephant_methods
 #' @export
 Summary.elephant <- Math.elephant
 
+#' @rdname elephant_methods
 #' @export
 `+.elephant` <- function(e1,e2) forget(e1) + forget(e2)
 
+#' @rdname elephant_methods
 #' @export
 `-.elephant` <- function(e1,e2) forget(e1) - forget(e2)
 
+#' @rdname elephant_methods
 #' @export
 `*.elephant` <- function(e1,e2) forget(e1) * forget(e2)
 
+#' @rdname elephant_methods
 #' @export
 `/.elephant` <- function(e1,e2) forget(e1) / forget(e2)
 
+#' @rdname elephant_methods
 #' @export
 `^.elephant` <- function(e1,e2) forget(e1) ^ forget(e2)
 
+#' @rdname elephant_methods
 #' @export
 `%%.elephant` <- function(e1,e2) forget(e1) %% forget(e2)
 
+#' @rdname elephant_methods
 #' @export
 `%/%.elephant` <- function(e1,e2) forget(e1) %/% forget(e2)
